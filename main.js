13d23bb7ee1f665f6fe69140861b33d61065b7d6
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-29 10:06:48 -0500

    Refactor functions

--- main.js
+++ main.js
@@ -15,30 +15,34 @@ sideButton.addEventListener('click', function(){
     seeFoodSuggestion()
     randomizeFoods(sides)
     selectButton(sideButton)
-})
+});
 
 mainButton.addEventListener('click', function () {
     getRandomIndex(mains)
     seeFoodSuggestion()
     randomizeFoods(mains)
     selectButton(mainButton)
-})
+});
 
 dessertButton.addEventListener('click', function () {
     getRandomIndex(desserts)
     seeFoodSuggestion()
     randomizeFoods(desserts)
     selectButton(dessertButton)
-})
+});
+
+entireMealButton.addEventListener('click', function() {
+    selectButton(entireMealButton)
+});
 
 letsCookButton.addEventListener('click', function() {
     getRandomIndex(sides)
     seeFoodSuggestion()
     randomMeal()
+    formatMealSuggestion()
 
-})
+});
 
-entireMealButton.addEventListener('click', selectEntireMeal)
 
 
 // Functions
@@ -47,9 +51,8 @@ function getRandomIndex(array) {
 }
 
 function randomizeFoods(type) {
-foodIdea.innerText = type[getRandomIndex(type)]
+    foodIdea.innerText = type[getRandomIndex(type)]
 }
-
 
 function seeFoodSuggestion() {
     potImageBox.classList.add("hidden")
@@ -63,34 +66,17 @@ function selectButton(type) {
     }
 }
 
-// function selectMain() {
-//     sideButton.checked = false
-//     dessertButton.checked = false
-//     mainButton.checked = true
-//     entireMealButton.checked = false
-// }
-
-// function selectDessert() {
-//     sideButton.checked = false
-//     dessertButton.checked = true
-//     mainButton.checked = false
-//     entireMealButton.checked = false
-// }
-
-function selectEntireMeal() {
-    sideButton.checked = false
-    dessertButton.checked = false
-    mainButton.checked = false
-    entireMealButton.checked = true
-}
-
 function randomMeal() {
     if (entireMealButton.checked) {
         foodIdea.innerText = `${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`
     }
-    foodIdea.style.fontSize= "20px"
-    foodIdea.style.margin= "20px"
 }
+
+function formatMealSuggestion() {
+    foodIdea.style.fontSize = "20px"
+    foodIdea.style.margin = "20px"
+}
+
 
 // Arrays
 var sides = [
